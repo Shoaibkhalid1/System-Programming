c4907611f19b13c01f1ec97cb48087359d0613e5
shoaib <shoaibkhalid804@yahoo.com>
2023-04-27 18:35:39 +0930

    added

---
+++ assignment2-prac.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    printf("Enter the number of arguments: %d",argc);
+    for(int i = 0;i<argc<i++){
+        printf("Argument %d : %s\n",i,argv[i]);
+    }
+    if(argc>1 && strcmp(argv[1], "-v")==0){
+        printf("mode enabled\n");
+    }
+    return 0;
+}
